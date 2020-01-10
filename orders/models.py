a0a578c189ae8029aa76ae19a8cff0103d3ea857
thomaswong25520 <thomaswong8321@gmail.com>
2020-01-10 22:13:23 +0100

    login, logout, registration added

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Topping(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return(f"{self.name}")
+    
+
+class Pizza(models.Model):
+    PIZZA_SIZES = (
+        ('S', 'Small'),
+        ('L', 'Large'),
+    )
+
+    pizza_type = models.CharField(max_length=64)
+    pizza_size = models.CharField(max_length=1, choices=PIZZA_SIZES)
+    toppings = models.ManyToManyField(Topping, default='Cheese')
+    price = models.IntegerField(help_text="Price in $")
+
+    def __str__(self):
+        return f"Size: {self.get_pizza_size_display()}, Type: {self.pizza_type}, Price: {self.price}, Toppings: {self.toppings.in_bulk()}"
+
+
--- pizza/urls.py
+++ pizza/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path("", include("orders.urls")),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    # path("", include("orders.urls")),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
